11790b6e0e07e21d4c31501ce988441584c6753a
Marco <nadaquehaceryt@gmail.com>
2026-05-21 15:41:44 -0700

    initial commit for proyect

---
+++ frontend_bien/AgrocapitalCotizador/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-zinc-900">
+      <h1 className="text-5xl font-bold text-cyan-400">
+        React + Vite + Tailwind + TS
+      </h1>
+    </div>
+  )
+}
+
+export default App
